c39aefef66c0f741039c5673d509df58c63884ac
beyondsociety <beyondsociety@gmail.com>
2015-04-01 15:13:15 -0600

    Updated rules64.mk

--- rules64.mk
+++ rules64.mk
@@ -1,7 +1,6 @@
 # 4/1/15 - the different CFLAGS/LDFLAGS/LIBS seems like a hack, need to create a build script for this
 
 # You will need a cross-compiler thats targeted for elf to compile and link the source code
-<<<<<<< HEAD
 TARGET_LOCATION  = $(HOME)/opt/cross64/bin
 TARGET_ARCH      = x86_64-elf
 
@@ -14,7 +13,7 @@ CC               = clang
                
 # CFLAGS for Cross-compiled Gcc
 #CFLAGS          = -O2 -ffreestanding -std=gnu99 -mcmodel=large -mno-red-zone -mno-mmx -mno-sse \
-#		   -mno-sse2 -mno-sse3 -mno-3dnow $(WARNINGS) $(INCLUDES) 
+#                  -mno-sse2 -mno-sse3 -mno-3dnow $(WARNINGS) $(INCLUDES) 
 
 # LDFLAGS for Clang
 LDFLAGS          = -zmax-page-size=0x1000 -Wl,-Map,info. map -nostdlib -Wl,-T,linker.ld $(LIBS)
@@ -35,32 +34,3 @@ WARNINGS         = -Wall -Wextra -Werror -pedantic -Wshadow -Wpointer-arith -Wca
                    -Wwrite-strings -Wmissing-prototypes -Wmissing-declarations \
                    -Wredundant-decls -Wnested-externs -Winline -Wno-long-long \
                    -Wuninitialized -Wstrict-prototypes -Wconversion 
-=======
-TARGET_LOCATION = $(HOME)/opt/cross64/bin 
-TARGET_ARCH  	 = x86_64-elf 
-
-CC              = clang
-#CC             = $(TARGET_LOCATION)/$(TARGET_ARCH)-gcc
-
-#CFLAGS         = -m64 -O2 -ffreestanding -std=gnu99 -nostdlib -mno-red-zone -Wl,-Map,info.map \
-#                 -mcmodel=large -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-3dnow \
-#                 -z max-page-size=0x1000 -Wl,-T,linker.ld -lgcc $(WARNINGS) $(INCLUDES)
-               
-CFLAGS          = -m64 -O2 -ffreestanding -std=gnu99 -mno-red-zone $(WARNINGS) $(INCLUDES) 
-
-LD              = $(TARGET_LOCATION)/$(TARGET_ARCH)-ld 
-LDFLAGS         = -z max-page-size=0x1000 -Map info.map -nostdlib -T linker.ld 
-
-ASM             = nasm  
-ASMFLAGS        = elf64
-
-INCLUDES        = -I ./include
-
-WARNINGS        = -Wall -Wextra -Werror -pedantic -Wshadow -Wpointer-arith -Wcast-align \
-                  -Wwrite-strings -Wmissing-prototypes -Wmissing-declarations \
-                  -Wredundant-decls -Wnested-externs -Winline -Wno-long-long \
-                  -Wuninitialized -Wstrict-prototypes -Wno-conversion 
->>>>>>> d512186d7ded4668bd471da12eb952300562b8c6
-
-
-
