4fc7ec8d1c4cbe2c9bad1612a8856a328abd6a1e
William McCabe <beyondsociety@gmail.com>
2017-07-30 21:01:35 -0600

    Changed the set.sh file

--- tools/vagrant/setup.sh
+++ tools/vagrant/setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # Download Vagrant and virtualbox
 
-# install virtualbox guest additions
+# Install virtualbox guest additions
 vagrant plugin install vagrant-vbguest
 
-# Update the box
+# Update the vagrant box
 vagrant box update
 
-# run the box and setup linux environment
+# Reload the vagrant box to fix issues with guest additons and virtualbox
+vagrant reload --no-provision
+
+# Start the vagrant box and setup our linux environment
 vagrant up --provision
 
-# ssh into vagrant box automatically
+# Ssh into the vagrant box automatically
 vagrant ssh
