93773521135296441e1d8499f8421ade0c565b4a
beyondsociety <beyondsociety@gmail.com>
2020-06-04 23:34:08 -0600

    Changed a few things for meson build system

--- scripts/run-cross.sh
+++ scripts/run-cross.sh
@@ -19,6 +19,6 @@ ninja -C cross-build
 
 echo ''
 echo "${Yellow}Building ISO Image...${Normal}"
-cp ./cross-build/src/kernel.elf ./iso/boot/
+cp ./cross-build/kernel/kernel.elf ./iso/boot/
 mkisofs -R -b boot/grub/eltorito.img -no-emul-boot -boot-load-size 4 \
 -boot-info-table -input-charset utf8 -o ./cross-build/boot.iso iso
