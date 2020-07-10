8160f5a543da7889c9d5fda6bfe4e507d196e98c
William McCabe <beyondsociety@gmail.com>
2020-07-09 19:53:26 -0600

    Updated grub-check script
    Forgot to add a "echo multiboot confirmed" to the script

--- tools/scripts/grub-check.sh
+++ tools/scripts/grub-check.sh
@@ -1,6 +1,7 @@
 if grub-file --is-x86-multiboot ./cross-build/kernel/kernel.elf; then
 	echo multiboot confirmed
- elif grub-file --is-x86-multiboot ./clang-build/kernel/kernel.elf; then
+elif grub-file --is-x86-multiboot ./clang-build/kernel/kernel.elf; then
+  echo multiboot confirmed
 else
 	echo file is not multiboot
 fi
