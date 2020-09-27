dc532714a11d31fc40f31e1796077315f91b5b7f
beyondsociety <beyondsociety@gmail.com>
2020-09-27 16:37:58 -0600

    Added directories to sperate firmware bios/efi files to make it easier to mange code.

--- tools/scripts/run-cross.sh
+++ tools/scripts/run-cross.sh
@@ -4,20 +4,23 @@ GREEN_TEXT='\033[1;32m'  # Bold Green
 NORMAL='\033[0;m'        # No color
 
 TARGET="i686-elf-gcc"
-#if ( find / -iname $TARGET -print -quit 2>/dev/null ); then
-#  echo "${GREEN_TEXT}Found cross-compiler: ${NORMAL}$TARGET"
-#else
-#  echo "Need cross-compiler to build Ibox... "
-#  exit 1
-#fi
+PREFIX="/usr/local/cross"
 
-if [ -f "$TARGET" ]; then
-  echo "${GREEN_TEXT}Found cross-compiler...${NORMAL}"
-  echo "$TARGET"
+if ( find / -type f -iname $TARGET -print -quit 2>/dev/null ); then
+  echo "${GREEN_TEXT}Found cross-compiler: ${NORMAL}$TARGET"
+  export PATH="$PREFIX/bin:$PATH"
 else
   echo "Need cross-compiler to build Ibox... "
   exit 1
 fi
+
+#if [ -d "$PREFIX" ]; then
+#  echo "${GREEN_TEXT}Found cross-compiler...${NORMAL}"
+#  echo "$PREFIX"
+#else
+#  echo "Need cross-compiler to build Ibox... "
+#  exit 1
+#fi
 
 echo ''
 echo "${GREEN_TEXT}Removing build directiory for new build...${NORMAL}"
@@ -31,6 +34,6 @@ ninja -C cross-build
 
 echo ''
 echo "${GREEN_TEXT}Building ISO Image...${NORMAL}"
-cp ./cross-build/kernel/kernel.elf ./iso/boot/
+cp ./cross-build/kernel.elf ./iso/boot/
 mkisofs -R -b boot/grub/eltorito.img -no-emul-boot -boot-load-size 4 \
 -boot-info-table -input-charset utf8 -o ./cross-build/boot.iso iso
