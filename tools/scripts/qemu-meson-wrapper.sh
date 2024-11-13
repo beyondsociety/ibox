15b9b762b7857c8eb239b9818b9a99ca03b04df6
William McCabe <beyondsociety@gmail.com>
2024-11-13 10:57:18 -0700

    Updated qemu-meson-wrapper.sh
    Fixed some comments.

--- tools/scripts/qemu-meson-wrapper.sh
+++ tools/scripts/qemu-meson-wrapper.sh
@@ -9,12 +9,12 @@ export LIBGL_ALWAYS_INDIRECT=1
 export NO_AT_BRIDGE=1
 
 # To run the X terminal in WSL, we must export the display which is different for versions 1 and 2.
-# If using WSL2 on Windows 10/11. will use the windows ip address to connect to the network host (nameserver) when using a third party
-#   X server to run. If we are using WSLG on WIndows 11, use the localhost (127.0.0.1) just like we would on WSL1.
+# If using WSL2 on Windows 10/11, we will use the windows ip address to connect to the network host (nameserver) when using a third party
+# X server to run. If we are using WSLG on WIndows 11, use the localhost (127.0.0.1) just like we would on WSL1.
 if [[ "$(</proc/sys/kernel/osrelease)" == *microsoft* ]]; then
   echo Running on WSl2
-  # We are using WSLg on windows 11, so we are using export DISPALY=:0. if we were using WSL2 on Windows 10/11 with a third-party 
-  #   X server, we would use "export DISPLAY=$(grep nameserver /etc/resolv.conf | awk '{print $2}'):0.0"
+  # We are using WSLg on windows 11, so we are using export DISPLAY=:0. If we were using WSL2 on Windows 10/11 with a third-party 
+  # X server, we would use "export DISPLAY=$(grep nameserver /etc/resolv.conf | awk '{print $2}'):0.0".
   # See https://askubuntu.com/questions/1299323/how-to-set-up-display-variable-for-wsl2-of-ubuntu-20 for more info.
   export DISPLAY=:0
 elif [[ "$(</proc/sys/kernel/osrelease)" == *Microsoft* ]]; then
